b80769915c4f3585c5c0439b8d276c6b48f295de
James Farrell <james@toucan.earth>
2024-02-20 11:03:06 +0100

    fix: remove projectId error

--- components/ProjectBanner.tsx
+++ components/ProjectBanner.tsx
@@ -12,7 +12,7 @@ export function ProjectBanner(
 ) {
   return (
     <div className="overflow-hidden rounded-3xl">
-      <Banner {...props} src={props.url} fallbackSrc={profileImageUrl} />
+      <Banner url={props.url} src={props.url} fallbackSrc={profileImageUrl} />
     </div>
   );
 }
